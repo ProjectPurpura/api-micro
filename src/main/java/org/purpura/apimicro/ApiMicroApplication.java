068fe9b8b3847c5e0542f4d3a61bb2b9cce857a9
rkhue <85773727+rkhue@users.noreply.github.com>
2025-08-11 12:02:46 -0300

    (feat) Add main application and test class for Spring Boot project initialization

---
+++ src/main/java/org/purpura/apimicro/ApiMicroApplication.java
@@ -0,0 +1,13 @@
+package org.purpura.apimicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMicroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiMicroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/purpura/apimicro/ApiMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package org.purpura.apimicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMicroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
