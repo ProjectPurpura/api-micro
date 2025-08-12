287496edfd29bd604c99ef6270fc83ac6ede9b0b
rkhue <85773727+rkhue@users.noreply.github.com>
2025-08-12 09:12:46 -0300

    (feat) Habilitando cache, configurando cache do redis para saber receber CepResponseDTO

--- src/main/java/org/purpura/apimicro/ApiMicroApplication.java
+++ src/main/java/org/purpura/apimicro/ApiMicroApplication.java
@@ -2,8 +2,10 @@ package org.purpura.apimicro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ApiMicroApplication {
 
     public static void main(String[] args) {
