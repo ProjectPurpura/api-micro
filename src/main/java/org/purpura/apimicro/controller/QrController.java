6f72ebc7f3491c81017e8c6e1bf2452bcea91089
rkhue <85773727+rkhue@users.noreply.github.com>
2025-08-13 20:55:36 -0300

    (feat) Add QR code generation endpoint in QrController

--- src/main/java/org/purpura/apimicro/controller/QrController.java
+++ src/main/java/org/purpura/apimicro/controller/QrController.java
@@ -1,11 +1,26 @@
 package org.purpura.apimicro.controller;
 
+import jakarta.validation.Valid;
+import org.purpura.apimicro.model.qr.QrCodeRequestDTO;
+import org.purpura.apimicro.service.qr.QrCodeService;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Validated
 @RestController
 @RequestMapping("/qr")
 public class QrController {
+
+    private final QrCodeService qrCodeService;
+
+    public QrController(QrCodeService qrCodeService) {
+        this.qrCodeService = qrCodeService;
+    }
+
+    @PostMapping(value = "/generate", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<byte[]> generate(@RequestBody @Valid QrCodeRequestDTO qrCodeRequestDTO) {
+        return ResponseEntity.ok(qrCodeService.generate(qrCodeRequestDTO));
+    }
 }
