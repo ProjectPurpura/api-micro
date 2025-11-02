af494650561a535f2ca558eeb4d573b478fcaa76
rkhue <85773727+rkhue@users.noreply.github.com>
2025-11-02 18:40:33 -0300

    (feat) Constantes para chaves redis

---
+++ src/main/java/org/purpura/apimicro/config/redis/RedisKeys.java
@@ -0,0 +1,7 @@
+package org.purpura.apimicro.config.redis;
+
+// Guardar as chaves REDIS em constantes
+public class RedisKeys {
+    public static final String CEP_CACHE = "cep";
+    public static final String CEP_VALID_CACHE = "cep:valid";
+}
